a9e7731c53a84a88063b755fe93a9d7924fe5e1c
Federico Rosa <Fede02rosa@gmail.com>
2022-11-30 20:20:08 +0100

    created template from AoCHelper

---
+++ 2022/Day/Day_01.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+
+namespace _2022.Day
+{
+    public class Day_01 : BaseDay
+    {
+        private readonly string _input;
+        public Day_01()
+        {
+            _input = File.ReadAllText(InputFilePath);
+        }
+
+        public override ValueTask<string> Solve_1() => new($"Solution 1 {ClassPrefix} {CalculateIndex()}");
+
+
+        public override ValueTask<string> Solve_2() => new("Solution 2");
+
+    }
+}
---
+++ 2022/Program.cs
@@ -0,0 +1,35 @@
+﻿
+Console.WriteLine("====================");
+Console.WriteLine("      AOC 2022      ");
+Console.WriteLine("       DOTNET       ");
+Console.WriteLine("====================");
+Console.WriteLine("SELECT:");
+Console.WriteLine("1 FOR SOLVE ALL DAYS");
+Console.WriteLine("2 FOR SOLVE LAST DAY");
+Console.WriteLine("");
+
+string? menu = Console.ReadLine();
+
+switch (menu)
+{
+    case "1":
+        await Solver.SolveAll(opt =>
+        {
+            opt.ElapsedTimeFormatSpecifier = "F3";
+        });
+        break;
+
+    case "2":
+        await Solver.SolveLast(opt =>
+        {
+            opt.ElapsedTimeFormatSpecifier = "F3";
+        });
+        break;
+
+    default:
+        Console.WriteLine("INVALID OPTION");
+        break;
+}
+
+
+
