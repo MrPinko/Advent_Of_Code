93d912e3d8af9eea30f4a4afa82d42ec307991b4
federico rosa <federico.rosa@dierresoftware.com>
2022-12-01 17:39:26 +0100

    improve Day_01

--- 2022/Day/Day_01.cs
+++ 2022/Day/Day_01.cs
@@ -1,25 +1,26 @@
 ﻿
 namespace _2022.Day
 {
-    public class Day_01 : BaseDay
-    {
+	public class Day_01 : BaseDay
+	{
 
-		private IEnumerable<IEnumerable<int>> _input;
+		private IEnumerable<int> _input;
 
 		public Day_01()
 		{
-			_input = File.ReadAllText(InputFilePath).Split("\r\n\r\n").Select(x => x.Split("\r\n").Select(int.Parse));
+			_input = File.ReadAllText(InputFilePath).Split("\r\n\r\n").Select(x => x.Split("\r\n").Select(int.Parse).Sum()).OrderDescending();
 		}
 
-        public override ValueTask<string> Solve_1()
-        {
-			var itemStash = _input.Select(elf => elf.Aggregate(0, (acc, value) => acc + value));
-			return new(itemStash.Max().ToString());
+		public override ValueTask<string> Solve_1()
+		{
+			var itemStash = _input.First();
+			return new(itemStash.ToString());
 		}
+
 
 		public override ValueTask<string> Solve_2()
-        {
-			var itemStash = _input.Select(elf => elf.Aggregate(0, (acc, value) => acc + value)).OrderDescending().Take(3).Sum();
+		{
+			var itemStash = _input.Take(3).Sum();
 			return new(itemStash.ToString());
 		}
 
