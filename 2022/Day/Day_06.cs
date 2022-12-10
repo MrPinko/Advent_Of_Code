004de6717b4c16284f588c3159ac4f06167905a5
Federico Rosa <Fede02rosa@gmail.com>
2022-12-10 23:14:23 +0100

    Day_06

---
+++ 2022/Day/Day_06.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2022.Day
+{
+	internal class Day_06 : BaseDay
+	{
+		private readonly string _input;
+
+		public Day_06()
+		{
+			_input = File.ReadAllText(InputFilePath);
+		}
+
+		public override ValueTask<string> Solve_1() => new(StartOfBlock(4).ToString());
+
+		public override ValueTask<string> Solve_2() => new(StartOfBlock(14).ToString());
+
+		int StartOfBlock(int l) =>
+		 Enumerable.Range(l, _input.Length).First(i => _input.Substring(i - l, l).ToHashSet().Count == l);
+	}
+}
